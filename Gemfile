ef7027b7c3d2f3caa90d57aaa9f44ebcdff49198
Andy Maleh <andy.am@gmail.com>
2020-07-18 21:09:01 -0400

    Extracted Nebula CDateTime into a Glimmer Custom Gem (glimmer-cw-cdatetime-nebula)

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source 'https://rubygems.org'
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem 'glimmer-dsl-swt', '~> 0.3.1' #, path: '../glimmer-dsl-swt'
+gem 'glimmer-cw-cdatetime-nebula', '~> 1.4.0.0' #, path: '../glimmer-cw-cdatetime'
 gem 'activerecord', '~> 5.2.4.3'
 gem 'activerecord-jdbcsqlite3-adapter', '~> 52.6', :platform => :jruby
 
--- app/are_we_there_yet.rb
+++ app/are_we_there_yet.rb
@@ -7,13 +7,9 @@ Bundler.require
 
 require 'db/db'
 
-require 'vendor/nebula/org.eclipse.nebula.cwt_1.1.0.jar'
-require 'vendor/nebula/org.eclipse.nebula.widgets.cdatetime_1.4.0.jar'
 require 'vendor/nebula/org.eclipse.nebula.widgets.ganttchart_1.0.0.jar'
 
 Glimmer::Config.import_swt_packages += [
-  'org.eclipse.nebula.cwt',
-  'org.eclipse.nebula.widgets.cdatetime',
   'org.eclipse.nebula.widgets.ganttchart',
   'org.eclipse.nebula.widgets.ganttchart.themes',
 ]
@@ -32,38 +28,6 @@ class AreWeThereYet
   }
   
   def open
-    configure_table_proxy
     app_view.open
-  end
-  
-  def configure_table_proxy
-    Glimmer::SWT::TableProxy.editors[:c_date_time] ||= {
-      widget_value_property: :selection,
-      editor_gui: lambda do |args, model, property, table_proxy|      
-        args << CDT::DROP_DOWN if args.empty?
-        table_editor_widget_proxy = c_date_time(*args) {
-          table_proxy.table_editor.minimumHeight = 20
-          selection model.send(property)
-          focus true
-          on_focus_lost {
-            table_proxy.finish_edit!
-          }
-          on_key_pressed { |key_event|
-            if key_event.keyCode == swt(:cr)
-              table_proxy.finish_edit!
-            elsif key_event.keyCode == swt(:esc)
-              table_proxy.cancel_edit!
-            end
-          }
-          on_widget_selected { |event|
-            table_proxy.finish_edit!
-          }
-          on_widget_default_selected { |event|
-            table_proxy.finish_edit!
-          }
-        }
-        table_editor_widget_proxy
-      end
-    }
   end
 end
